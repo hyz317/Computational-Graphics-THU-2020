cac5044cbddeba897ad5cdc4b2e46e8aed061e12
何雨泽 <hyz18@mails.tsinghua.edu.cn>
2020-04-05 00:44:00 +0800

    PA3 framework

---
+++ graphics_PA3/include/curve.hpp
@@ -0,0 +1,96 @@
+#ifndef CURVE_HPP
+#define CURVE_HPP
+
+#include "object3d.hpp"
+#include <vecmath.h>
+#include <vector>
+#include <utility>
+
+#include <algorithm>
+
+// TODO (PA3): Implement Bernstein class to compute spline basis function.
+//       You may refer to the python-script for implementation.
+
+// The CurvePoint object stores information about a point on a curve
+// after it has been tesselated: the vertex (V) and the tangent (T)
+// It is the responsiblility of functions that create these objects to fill in all the data.
+struct CurvePoint {
+    Vector3f V; // Vertex
+    Vector3f T; // Tangent  (unit)
+};
+
+class Curve : public Object3D {
+protected:
+    std::vector<Vector3f> controls;
+public:
+    explicit Curve(std::vector<Vector3f> points) : controls(std::move(points)) {}
+
+    bool intersect(const Ray &r, Hit &h, float tmin) override {
+        return false;
+    }
+
+    std::vector<Vector3f> &getControls() {
+        return controls;
+    }
+
+    virtual void discretize(int resolution, std::vector<CurvePoint>& data) = 0;
+
+    void drawGL() override {
+        Object3D::drawGL();
+        glPushAttrib(GL_ALL_ATTRIB_BITS);
+        glDisable(GL_LIGHTING);
+        glColor3f(1, 1, 0);
+        glBegin(GL_LINE_STRIP);
+        for (auto & control : controls) { glVertex3fv(control); }
+        glEnd();
+        glPointSize(4);
+        glBegin(GL_POINTS);
+        for (auto & control : controls) { glVertex3fv(control); }
+        glEnd();
+        std::vector<CurvePoint> sampledPoints;
+        discretize(30, sampledPoints);
+        glColor3f(1, 1, 1);
+        glBegin(GL_LINE_STRIP);
+        for (auto & cp : sampledPoints) { glVertex3fv(cp.V); }
+        glEnd();
+        glPopAttrib();
+    }
+};
+
+class BezierCurve : public Curve {
+public:
+    explicit BezierCurve(const std::vector<Vector3f> &points) : Curve(points) {
+        if (points.size() < 4 || points.size() % 3 != 1) {
+            printf("Number of control points of BezierCurve must be 3n+1!\n");
+            exit(0);
+        }
+    }
+
+    void discretize(int resolution, std::vector<CurvePoint>& data) override {
+        data.clear();
+        // TODO (PA3): fill in data vector
+    }
+
+protected:
+
+};
+
+class BsplineCurve : public Curve {
+public:
+    BsplineCurve(const std::vector<Vector3f> &points) : Curve(points) {
+        if (points.size() < 4) {
+            printf("Number of control points of BspineCurve must be more than 4!\n");
+            exit(0);
+        }
+    }
+
+    void discretize(int resolution, std::vector<CurvePoint>& data) override {
+        data.clear();
+        // TODO (PA3): fill in data vector
+    }
+
+protected:
+
+};
+
+#endif // CURVE_HPP
